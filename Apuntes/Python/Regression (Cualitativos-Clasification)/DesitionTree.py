91db09fad26a482b4296fe0902c464535edb3a87
Aarón Juárez <66045880+Aaronga19@users.noreply.github.com>
2020-11-24 17:16:23 -0600

    Algoritmos de árboles y bosques de desición añadidos (Clasificatorios terminados)

---
+++ Apuntes/Python/Regression (Cualitativos-Clasification)/DesitionTree.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 24 11:52:38 2020
+
+@author: Aaronga
+"""
+
+"""                                     Arbol de Desición                                          """
+
+# Se importa el Dataset__
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Cargar el conjunto de datos y dividirlos por variable independiente variable(s) independientes (s)
+
+dataset = pd.read_csv("C:/Users/Aaronga/Documents/GitHub/MachineLearningAZ/datasets/Part 3 - Classification\Section 14 - Logistic Regression/Social_Network_Ads.csv")
+X = dataset.iloc[:,[2,3]].values # Para convertirlo a vector se usa [:,1:2]
+y = dataset.iloc[:, 4].values
+
+# Dicidir el data set en conjunto de entrenamiento y conjunto de testing
+                       
+from sklearn.model_selection import train_test_split
+XTrain, XTest, YTrain, YTest = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+
+# Escalado de variables 
+"""
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+XTrain = sc_X.fit_transform(XTrain)
+XTest = sc_X.transform(XTest)
+"""
+# Ajustar el clasificador
+
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion="entropy", random_state=0)
+classifier.fit(XTrain, YTrain)
+
+# Predicción del Clasificador 
+
+y_pred = classifier.predict(XTest)
+
+# Elaborar una matriz de confusión 
+from sklearn.metrics import confusion_matrix 
+
+cm = confusion_matrix(YTest, y_pred) # Así podemos tener una breve descripcion de como es el modelo, en donde acertó y cuantas veces se equivocó 
+
+# Visualizacion de los datos del Clasificador
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = XTrain, YTrain
+X1, X2 = np.meshgrid(np.arange(start = X_set[:,0].min()-1,stop=X_set[:,0].max()+1, step= 1),
+                     np.arange(start = X_set[:,1].min()-1,stop=X_set[:,1].max()+1, step= 500))# Particiones de las gráficas
+plt.contourf(X1,X2, classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red','green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i,j in enumerate (np.unique(y_set)):
+    plt.scatter(X_set[y_set== j,0], X_set[y_set==j,1],
+                c = ListedColormap(('red','green'))(i),label = j)
+plt.title("Arbol de Desición (Training set)")
+plt.xlabel("Age")
+plt.ylabel("Estimated Salary")
+plt.legend()
+plt.show()
+
+
+
+X_set, y_set = XTest, YTest
+X1, X2 = np.meshgrid(np.arange(start = X_set[:,0].min()-1,stop=X_set[:,0].max()+1, step= 1),
+                     np.arange(start = X_set[:,1].min()-1,stop=X_set[:,1].max()+1, step= 500))
+plt.contourf(X1,X2, classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red','green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i,j in enumerate (np.unique(y_set)):
+    plt.scatter(X_set[y_set== j,0], X_set[y_set==j,1],
+                c = ListedColormap(('red','green'))(i),label = j)
+plt.title("Arbol de Desición (Testing set)")
+plt.xlabel("Age")
+plt.ylabel("Estimated Salary")
+plt.legend()
+plt.show()
+
---
+++ Apuntes/R/Clasification/DesitionTree.R
@@ -0,0 +1,99 @@
+
+
+
+#                                        Arboles de Desición 
+
+# Se carga el conjunto de datos
+
+dataset = read.csv("GitHub/MachineLearningAZ/datasets/Part 3 - Classification/Section 14 - Logistic Regression/Social_Network_Ads.csv")
+# Filtrado de columnas si es necesario
+dataset = dataset[, 3:5]
+
+# Colocar la variable de clasificacion como factor ya que Bayes lo pide en la librería 
+
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c(0,1)) 
+
+
+
+# codificar variables categoricas
+
+""" 
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1,2,3))
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c('No','Yes'),
+                           labels = c(0,1))
+"""
+
+# Dividir los datos en conjunto de entrenamiento y conjunto de test
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+split
+trainingSet = subset(dataset, split == T)
+testingSet = subset(dataset, split == F)
+
+
+# Escalado de valores 
+
+"""
+trainingSet[,1:2] = scale(trainingSet[,1:2])
+testingSet[,1:2] = scale(testingSet[,1:2])
+"""
+
+# Ajustar modelo de Árbol de desición con el conjunto de entrenamiento 
+
+library(rpart)
+classifier = rpart(formula = Purchased ~ .,
+                   data = trainingSet,
+)
+
+
+# Prediccion de nuevos resultados con el conjunto de Testing
+y_pred = predict(classifier, newdata = testingSet[,-3], 
+                 type = "class") # Class para que me de resultadoes en 0 o 1 
+
+
+# Crear matriz de Confusión 
+
+cm = table(testingSet[,3], y_pred)
+
+# Visualizacion del conjunto de entrenamiento
+install.packages("ElemStatLearn")
+library(ElemStatLearn)
+set = trainingSet
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 200)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set, type = "class")
+plot(set[, -3],
+     main = 'Árbol de Desición (Conjunto de Entrenamiento)',
+     xlab = 'Edad', ylab = 'Sueldo Estimado',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+
+# Visualización del conjunto de testing
+set = testingSet
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 200)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set, type = "class")
+plot(set[, -3],
+     main = 'Árbol de Desición (Conjunto de Testing)',
+     xlab = 'Edad', ylab = 'Sueldo Estimado',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# Representación del árbol clasificador 
+plot(classifier)
+text(classifier)
